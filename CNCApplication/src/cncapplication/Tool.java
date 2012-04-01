b507cec54a4f27e187c2df375b66dc1673515198
mwaldron74 <mwaldron74@yahoo.com>
2012-04-01 18:01:22 -0400

    Tool class added

---
+++ CNCApplication/src/cncapplication/Tool.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cncapplication;
+
+/**
+ *
+ * @author mwaldron74
+ */
+public class Tool {
+    private int x, y, z, diameter, height;
+    public Tool(int x, int y, int z, int diameter, int height)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.diameter = diameter;
+        this.height = height;
+    }
+
+}
